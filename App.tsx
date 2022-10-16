e384e4fcfaf09018d3c1fe015fb0b35c1370dee0
shireesha <ps.shireesha@gmail.com>
2022-10-16 17:52:11 +0100

    make initial setup

--- App.tsx
+++ App.tsx
@@ -1,29 +1,82 @@
-import React from 'react';
-import type { ReactElement } from 'react';
-import { Text, View } from 'react-native';
-import styles from './App.styles';
+import React, { useState } from 'react';
+import { View, Text, FlatList, useWindowDimensions, Image, StyleSheet, Switch } from 'react-native';
 
-const App: () => ReactElement = () => {
+let magazineData = require('./data.json');
+
+const styles = StyleSheet.create({
+    screenTitle: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        letterSpacing: 0.44,
+        marginBottom: 0,
+        paddingTop: 50,
+				paddingLeft: 24,
+    },
+    portrait: {
+        textAlign: 'center',
+        paddingTop: 10,
+        borderRadius: 4,
+    },
+    coverImage: {
+        width: 'auto',
+        height: 260,
+        overflow: 'hidden',
+    },
+
+    coverTitle: {
+        fontSize: 21,
+        lineHeight: 28,
+    },
+
+    landscape: {
+        flexDirection: 'row',
+        display: 'flex',
+        flexWrap: 'wrap',
+        justifyContent: 'space-between',
+    },
+});
+
+magazineData = Object.keys(magazineData.issues).map((key) => [magazineData.issues[key]]);
+magazineData = magazineData.map((item, index) => ({ ...item, id: index + 1 }));
+
+const App = (props) => {
+    const { width, height } = useWindowDimensions();
+    const isPortrait = height > width;
+
+    const [isEnabled, setIsEnabled] = useState(true);
+    const toggleSwitch = () => setIsEnabled((previousState) => !previousState);
+
     return (
-        <View style={styles.container}>
-            <Text style={styles.title}>Mobile Tech Test</Text>
-            <Text style={styles.text}>Please replace this with your solution.</Text>
-            <Text style={styles.subtitle}>Description:</Text>
-            <Text style={styles.text}>
-                The screen must have the title "Magazine issues". We would like you to display the data contained in
-                data.json. Between the title and the data, you should create 4 toggles which filter based on cover name.
-                The default is that all 4 are switched on. Switching off a toggle should hide all instances of that
-                issue type. Switching it back on should bring them back. We would like you to include a footnote at the
-                bottom "The full archive is available to Which? members". The whole screen should be scrollable, not
-                just the data.
-            </Text>
-            <Text style={styles.subtitle}>Constraints:</Text>
-            <Text style={styles.text}>
-                You should consider mobile portrait, mobile landscape and tablet devices. Mobile portrait will have one
-                item per row. Mobile landscape will have 2 items per row. Tablet will have 3 items per row with the
-                content centered. The content should not touch the edge of the screen. You should take care to account
-                for phone notches.
-            </Text>
+        <View>
+            <Text style={styles.screenTitle}>Magazine Issues</Text>
+            <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+                <Text>{isEnabled ? 'Magazines' : ''}</Text>
+                <Switch style={{ marginTop: 30 }} value={isEnabled} onValueChange={toggleSwitch} />
+                <Text>{isEnabled ? 'Gardening' : ''}</Text>
+                <Switch style={{ marginTop: 30 }} value={isEnabled} onValueChange={toggleSwitch} />
+                <Text>{isEnabled ? 'Travel' : ''}</Text>
+                <Switch style={{ marginTop: 30 }} value={isEnabled} onValueChange={toggleSwitch} />
+                <Text>{isEnabled ? 'Computing' : ''}</Text>
+                <Switch style={{ marginTop: 30 }} value={isEnabled} onValueChange={toggleSwitch} />
+            </View>
+            <FlatList
+                data={magazineData}
+                keyExtractor={(item) => item.id.toString()}
+                renderItem={({ item }) => {
+                    return (
+                        <View style={isPortrait ? styles.portrait : styles.landscape}>
+                            <Text>{item[0].issue}</Text>
+                            <Text style={styles.coverTitle}>{item[0].cover}</Text>
+                            <Image
+                                style={styles.coverImage}
+                                source={{
+                                    uri: item[0].uri,
+                                }}
+                            />
+                        </View>
+                    );
+                }}
+            />
         </View>
     );
 };
