45499edf7c597d81d2e89aedaf9ece78ba506be7
shireesha <ps.shireesha@gmail.com>
2022-10-17 08:38:03 +0100

    add folder structure

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, Text, FlatList, useWindowDimensions, Image, StyleSheet, Switch } from 'react-native';
+import { View, Text, FlatList, useWindowDimensions, Image, StyleSheet, Switch, ScrollView } from 'react-native';
 
 let magazineData = require('./data.json');
 
@@ -10,7 +10,7 @@ const styles = StyleSheet.create({
         letterSpacing: 0.44,
         marginBottom: 0,
         paddingTop: 50,
-				paddingLeft: 24,
+        paddingLeft: 24,
     },
     portrait: {
         textAlign: 'center',
@@ -29,28 +29,34 @@ const styles = StyleSheet.create({
     },
 
     landscape: {
-        flexDirection: 'row',
-        display: 'flex',
-        flexWrap: 'wrap',
-        justifyContent: 'space-between',
+			display: 'flex'
+    },
+
+    itemLandscape: {
+        width: '50%', // is 50% of container width
     },
 });
 
 magazineData = Object.keys(magazineData.issues).map((key) => [magazineData.issues[key]]);
 magazineData = magazineData.map((item, index) => ({ ...item, id: index + 1 }));
 
-const App = (props) => {
+const App = () => {
     const { width, height } = useWindowDimensions();
     const isPortrait = height > width;
 
     const [isEnabled, setIsEnabled] = useState(true);
+    // const [magazineData, setMagazineData] = useState([]);
+    const [travelData, setTravelData] = useState([]);
+    const [computingData, setComputingData] = useState([]);
+    const [gardeningData, setGardeningData] = useState([]);
+
     const toggleSwitch = () => setIsEnabled((previousState) => !previousState);
 
     return (
-        <View>
+        <ScrollView stickyHeaderIndices={[4]}>
             <Text style={styles.screenTitle}>Magazine Issues</Text>
             <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
-                <Text>{isEnabled ? 'Magazines' : ''}</Text>
+                <Text>{isEnabled ? 'Magazine' : ''}</Text>
                 <Switch style={{ marginTop: 30 }} value={isEnabled} onValueChange={toggleSwitch} />
                 <Text>{isEnabled ? 'Gardening' : ''}</Text>
                 <Switch style={{ marginTop: 30 }} value={isEnabled} onValueChange={toggleSwitch} />
@@ -64,7 +70,7 @@ const App = (props) => {
                 keyExtractor={(item) => item.id.toString()}
                 renderItem={({ item }) => {
                     return (
-                        <View style={isPortrait ? styles.portrait : styles.landscape}>
+                        <View style={isPortrait ? styles.portrait : styles.itemLandscape}>
                             <Text>{item[0].issue}</Text>
                             <Text style={styles.coverTitle}>{item[0].cover}</Text>
                             <Image
@@ -77,7 +83,10 @@ const App = (props) => {
                     );
                 }}
             />
-        </View>
+            <View style={{ flex: 1, backgroundColor: '#fff5d4', alignItems: 'center', justifyContent: 'center', height: 100, width: '100%' }}>
+                <Text>The full archive is available to Which? members</Text>
+            </View>
+        </ScrollView>
     );
 };
 
